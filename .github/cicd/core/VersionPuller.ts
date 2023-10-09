819a9e74554e22803c210cb82173f41f79b5a50d
Calvin Wilkinson <kinsondigital@gmail.com>
2023-10-09 15:33:06 +0100

    config: add import map

--- .github/cicd/core/VersionPuller.ts
+++ .github/cicd/core/VersionPuller.ts
@@ -1,5 +1,5 @@
 import { Utils } from "core/Utils.ts";
-import { Directory } from "./Directory.ts";
+import { Directory } from "cicd-core/Directory.ts";
 
 /**
  * Pulls the version from a json file.
