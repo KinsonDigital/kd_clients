79740956d4f872f1ae1ff0d009757a6f5073aa9f
Calvin Wilkinson <kinsondigital@gmail.com>
2023-10-14 18:46:52 +0100

    refactor: update deps imports

--- deps.ts
+++ deps.ts
@@ -1,6 +1,6 @@
 import { decodeBase64, encodeBase64 } from "https://deno.land/std@0.203.0/encoding/base64.ts";
-import { existsSync } from "https://deno.land/std@0.203.0/fs/mod.ts";
-import { assertEquals } from "https://deno.land/std@0.203.0/assert/mod.ts";
+import { existsSync } from "https://deno.land/std@0.203.0/fs/exists.ts";
+import { assertEquals } from "https://deno.land/std@0.203.0/assert/assert_equals.ts";
 import { TweetV2PostTweetResult, TwitterApi } from "npm:twitter-api-v2@1.15.0";
 
 export { decodeBase64, encodeBase64 };
