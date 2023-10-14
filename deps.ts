8feb19985909512c515d3b0f09ec1cab6ae772b3
Calvin Wilkinson <kinsondigital@gmail.com>
2023-10-14 18:47:47 +0100

    deps: add dependency to npm chalk library

--- deps.ts
+++ deps.ts
@@ -2,9 +2,11 @@ import { decodeBase64, encodeBase64 } from "https://deno.land/std@0.203.0/encodi
 import { existsSync } from "https://deno.land/std@0.203.0/fs/exists.ts";
 import { assertEquals } from "https://deno.land/std@0.203.0/assert/assert_equals.ts";
 import { TweetV2PostTweetResult, TwitterApi } from "npm:twitter-api-v2@1.15.0";
+import chalk from "npm:chalk@4.1.1";
 
 export { decodeBase64, encodeBase64 };
 export { existsSync };
 export { assertEquals };
 export { TwitterApi };
 export type { TweetV2PostTweetResult };
+export { chalk };
