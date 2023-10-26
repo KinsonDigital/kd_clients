cc7c4af9ffc7b3f2bf8f76ccc0bd82febbeaa6f3
Calvin Wilkinson <kinsondigital@gmail.com>
2023-10-26 18:35:50 +0100

    config: improve dependencies

--- deps.ts
+++ deps.ts
@@ -1,12 +1,14 @@
+import chalk from "npm:chalk@4.1.1";
 import { decodeBase64, encodeBase64 } from "https://deno.land/std@0.203.0/encoding/base64.ts";
 import { existsSync } from "https://deno.land/std@0.203.0/fs/exists.ts";
-import { assertEquals } from "https://deno.land/std@0.203.0/assert/assert_equals.ts";
 import { TweetV2PostTweetResult, TwitterApi } from "npm:twitter-api-v2@1.15.0";
-import chalk from "npm:chalk@4.1.1";
+import { assert, assertEquals, assertThrows, assertRejects, equal } from "https://deno.land/std@0.204.0/assert/mod.ts";
+import { assertSpyCall, assertSpyCalls, spy, stub, returnsNext, returnsArg } from "https://deno.land/std@0.204.0/testing/mock.ts";
 
 export { decodeBase64, encodeBase64 };
 export { existsSync };
-export { assertEquals };
 export { TwitterApi };
 export type { TweetV2PostTweetResult };
 export { chalk };
+export { assert, assertEquals, assertThrows, assertRejects, equal };
+export { assertSpyCall, assertSpyCalls, spy, stub, returnsNext, returnsArg }
