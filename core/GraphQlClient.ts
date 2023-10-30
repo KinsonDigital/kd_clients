641192b4cbf83b289549ea1f84bd76d1df8e6ca6
Calvin Wilkinson <kinsondigital@gmail.com>
2023-10-30 11:43:26 +0000

    cleanup: remove unused protected funcs

--- core/GraphQlClient.ts
+++ core/GraphQlClient.ts
@@ -69,11 +69,10 @@ export abstract class GraphQlClient {
 	/**
 	 * Gets the response data from a request.
 	 * @param response The response from a request.
-	 * @param throwWithErrors Whether or not to throw and error if the response contains errors.
 	 * @returns The response data.
 	 * @remarks This method will throw an error if the response contains errors.
 	 */
-	protected async getResponseData(response: Response, throwWithErrors = true): Promise<RequestResponseModel> {
+	protected async getResponseData(response: Response): Promise<RequestResponseModel> {
 		const responseText = await response.text();
 		const responseData = await JSON.parse(responseText);
 
@@ -102,58 +101,5 @@ export abstract class GraphQlClient {
 		}
 
 		return result;
-	}
-
-	/**
-	 * Gets all of the errors from the response data.
-	 * @param responseData The response data from the request.
-	 * @returns The list of errors.
-	 */
-	private getErrors(responseData: RequestResponseModel): ErrorModel[] {
-		if (this.containsErrors(responseData)) {
-			const errors: ErrorModel[] | undefined = responseData["errors"];
-
-			if (errors === undefined) {
-				return [];
-			}
-
-			return errors;
-		}
-
-		return [];
-	}
-
-	/**
-	 * Returns a value indicating whether or not the given {@link responseData} contains any errors.
-	 * @param response The response to check if it contains an errors object.
-	 * @returns True if the object contains errors, false otherwise.
-	 */
-	private containsErrors(
-		responseData: RequestResponseModel,
-		key = "errors",
-	): responseData is RequestResponseModel & { [k in typeof key]: string } {
-		return key in responseData;
-	}
-
-	/**
-	 * Returns a value indicating whether or not the given object is a bad credentials object.
-	 * @param responseOrBadCreds The response or bad credentials object to check.
-	 * @returns True if the object is a bad credentials object, false otherwise.
-	 */
-	private isBadCredentialError(
-		responseOrBadCreds: RequestResponseModel | BadCredentials,
-	): responseOrBadCreds is BadCredentials {
-		return "documentation_url" in responseOrBadCreds &&
-			"message" in responseOrBadCreds &&
-			responseOrBadCreds.message === "Bad credentials";
-	}
-
-	/**
-	 * Gets a value indicating whether or not the given object is a Response object.
-	 * @param responseOrString The Response or string object to check.
-	 * @returns True if the object is a Response object, false otherwise.
-	 */
-	private isResponse(responseOrString: Response | string): responseOrString is Response {
-		return responseOrString instanceof Response;
 	}
 }
