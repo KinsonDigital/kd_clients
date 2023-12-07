e7a3130d23fa45f24498ff669f99f9cab3a837d2
Calvin Wilkinson <kinsondigital@gmail.com>
2023-12-07 12:14:13 +0000

    deps: add dependencies and rearrange imports and exports

--- deps.ts
+++ deps.ts
@@ -1,14 +1,26 @@
-import chalk from "npm:chalk@4.1.1";
-import { decodeBase64, encodeBase64 } from "https://deno.land/std@0.203.0/encoding/base64.ts";
+// ----IMPORTS----
+
+// Official Deno Modules
 import { existsSync } from "https://deno.land/std@0.203.0/fs/exists.ts";
-import { TweetV2PostTweetResult, TwitterApi } from "npm:twitter-api-v2@1.15.0";
+import { extname, basename } from "https://deno.land/std@0.203.0/path/mod.ts";
+import { decodeBase64, encodeBase64 } from "https://deno.land/std@0.203.0/encoding/base64.ts";
 import { assert, assertEquals, assertThrows, assertRejects, equal } from "https://deno.land/std@0.204.0/assert/mod.ts";
 import { assertSpyCall, assertSpyCalls, spy, stub, returnsNext, returnsArg } from "https://deno.land/std@0.204.0/testing/mock.ts";
 
-export { decodeBase64, encodeBase64 };
+// NPM Modules
+import chalk from "npm:chalk@4.1.1";
+import { TwitterApi, TweetV2PostTweetResult } from "npm:twitter-api-v2@1.15.0";
+
+// ----EXPORTS----
+
+// Official Deno Modules
 export { existsSync };
-export { TwitterApi };
-export type { TweetV2PostTweetResult };
-export { chalk };
+export { extname, basename };
+export { decodeBase64, encodeBase64 };
 export { assert, assertEquals, assertThrows, assertRejects, equal };
 export { assertSpyCall, assertSpyCalls, spy, stub, returnsNext, returnsArg }
+
+// NPM Modules
+export { chalk };
+export { TwitterApi };
+export type { TweetV2PostTweetResult };
