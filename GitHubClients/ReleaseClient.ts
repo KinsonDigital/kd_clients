0e78a55c7f97a5490c50f4c0c1040a7ca364a5a9
Calvin Wilkinson <kinsondigital@gmail.com>
2023-12-07 15:43:28 +0000

    feat: add function to get a release to replace other functions

--- GitHubClients/ReleaseClient.ts
+++ GitHubClients/ReleaseClient.ts
@@ -4,6 +4,7 @@ import { Guard } from "../core/Guard.ts";
 import { ReleaseModel } from "../core/Models/mod.ts";
 import { Utils } from "../core/Utils.ts";
 import { ReleaseError } from "./Errors/ReleaseError.ts";
+import { ReleaseOptions } from "./ReleaseOptions.ts";
 
 /**
  * Provides a client for interacting with GitHub releases.
@@ -56,6 +57,47 @@ export class ReleaseClient extends GitHubClient {
 		}
 
 		return [<ReleaseModel[]> await this.getResponseData(response), response];
+	}
+
+	/**
+	 * Gets a release for a repository.
+	 * @param getByValue The tag or title of the release to get.
+	 * @param options Various options to use when getting the release.
+	 * @returns The release for a repository.
+	 * @throws A {@link ReleaseError} if there was an issue getting the release or if it was not found.
+	 */
+	public async getRelease(getByValue: string, options?: ReleaseOptions): Promise<ReleaseModel> {
+		Guard.isNothing(getByValue, "getRelease", "getByValue");
+
+		getByValue = getByValue.trim();
+
+		const filterPredicate: (item: ReleaseModel) => boolean = (item: ReleaseModel) => {
+			return options?.getByTitle === true
+				? item.name === getByValue
+				: item.tag_name === getByValue;
+		};
+
+		const releases = await this.getAllDataUntil<ReleaseModel>(
+			async (page: number, qtyPerPage?: number) => {
+				return await this.getReleases(page, qtyPerPage ?? 100);
+			},
+			1, // Start page
+			100, // Qty per page
+			(pageOfData: ReleaseModel[]) => {
+				// If a release with the tag has been found, stop getting data.
+				return pageOfData.some(filterPredicate);
+			},
+		);
+
+		const foundRelease: ReleaseModel | undefined = releases.find(filterPredicate);
+
+		if (foundRelease === undefined) {
+			// TODO: Change message based if by tag or title
+			const errorMsg = `A release with the tag '${getByValue}' for the repository '${this.repoName}' could not be found.`;
+			throw new ReleaseError(errorMsg);
+		}
+
+		return foundRelease;
 	}
 
 	/**
