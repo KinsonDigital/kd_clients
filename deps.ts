cff682dd852a309ae1b3af20fc79a0a30abc8195
Calvin Wilkinson <kinsondigital@gmail.com>
2024-10-12 17:02:04 +0100

    🚧Upgrade to deno v2.0 (#88)
    * Start work for issue #87
    
    * config: update deno permissions in launch.json
    
    * config: update deno permissions in tasks.json
    
    * ci: update deno permissions in workflows
    
    * config: update deno tasks
    
    * refactor: fix linting errors
    
    * reformat: reformat code to meet coding standards

--- deps.ts
+++ deps.ts
@@ -1,11 +1,11 @@
 // ----IMPORTS----
 
 // Official Deno Modules
-import { exists, existsSync, walkSync, ensureDirSync } from "https://deno.land/std@0.224.0/fs/mod.ts";
-import { extname, basename, isAbsolute } from "https://deno.land/std@0.224.0/path/mod.ts";
+import { ensureDirSync, exists, existsSync, walkSync } from "https://deno.land/std@0.224.0/fs/mod.ts";
+import { basename, extname, isAbsolute } from "https://deno.land/std@0.224.0/path/mod.ts";
 import { decodeBase64, encodeBase64 } from "https://deno.land/std@0.224.0/encoding/base64.ts";
-import { assert, assertEquals, assertThrows, assertRejects, equal } from "https://deno.land/std@0.224.0/assert/mod.ts";
-import { assertSpyCall, assertSpyCalls, spy, stub, returnsNext, returnsArg } from "https://deno.land/std@0.224.0/testing/mock.ts";
+import { assert, assertEquals, assertRejects, assertThrows, equal } from "https://deno.land/std@0.224.0/assert/mod.ts";
+import { assertSpyCall, assertSpyCalls, returnsArg, returnsNext, spy, stub } from "https://deno.land/std@0.224.0/testing/mock.ts";
 import { oauth1a } from "jsr:@nexterias/twitter-api-fetch@3.0.1";
 
 // Local Core Types
@@ -14,28 +14,60 @@ import { GraphQlClient } from "./core/GraphQlClient.ts";
 
 // Local Core Models
 import {
-	CommitModel, FileContentModel, GitHubVariablesModel, GitHubVarModel, IssueModel, LabelModel, MilestoneModel,
-	ProjectModel, PullRequestHeadOrBaseModel, PullRequestInfoModel, PullRequestModel, ReleaseModel, RepoModel,
-	TagModel, UserModel, WorkflowRunModel, WorkflowRunsModel, AssetModel
-} from "./core/Models/mod.ts"
+	AssetModel,
+	CommitModel,
+	FileContentModel,
+	GitHubVariablesModel,
+	GitHubVarModel,
+	IssueModel,
+	LabelModel,
+	MilestoneModel,
+	ProjectModel,
+	PullRequestHeadOrBaseModel,
+	PullRequestInfoModel,
+	PullRequestModel,
+	ReleaseModel,
+	RepoModel,
+	TagModel,
+	UserModel,
+	WorkflowRunModel,
+	WorkflowRunsModel,
+} from "./core/Models/mod.ts";
 
 // Local GraphQL Models
-import { 
-	ErrorModel, GitBranchModel,	GraphQlRequestResponseModel, PageInfoModel,
-	LocationModel,RawGetBranchTargetModel, RawGitBranchModel, RawRefsGetBranchModel
+import {
+	ErrorModel,
+	GitBranchModel,
+	GraphQlRequestResponseModel,
+	LocationModel,
+	PageInfoModel,
+	RawGetBranchTargetModel,
+	RawGitBranchModel,
+	RawRefsGetBranchModel,
 } from "./core/Models/GraphQlModels/mod.ts";
 
 // Local GitHub Client Errors
 import {
-	AuthError, GitError, IssueError, LabelError, MilestoneError, OrganizationError,
-	ProjectError, PullRequestError, ReleaseError, RepoError, TagError, UsersError, WorkflowError,
-} from "./GitHubClients/Errors/mod.ts"
+	AuthError,
+	GitError,
+	IssueError,
+	LabelError,
+	MilestoneError,
+	OrganizationError,
+	ProjectError,
+	PullRequestError,
+	ReleaseError,
+	RepoError,
+	TagError,
+	UsersError,
+	WorkflowError,
+} from "./GitHubClients/Errors/mod.ts";
 
 // Package Client Errors
-import { NuGetError } from "./PackageClients/Errors/NuGetError.ts"
+import { NuGetError } from "./PackageClients/Errors/NuGetError.ts";
 
 // Other Client Errors
-import { XError } from "./OtherClients/Errors/XError.ts"
+import { XError } from "./OtherClients/Errors/XError.ts";
 
 // Local
 import { Utils } from "./core/Utils.ts";
@@ -45,11 +77,11 @@ import { GitHubHttpStatusCodes } from "./core/Enums.ts";
 // ----EXPORTS----
 
 // Official Deno Modules
-export { exists, existsSync, walkSync, ensureDirSync };
-export { extname, basename, isAbsolute };
+export { ensureDirSync, exists, existsSync, walkSync };
+export { basename, extname, isAbsolute };
 export { decodeBase64, encodeBase64 };
-export { assert, assertEquals, assertThrows, assertRejects, equal };
-export { assertSpyCall, assertSpyCalls, spy, stub, returnsNext, returnsArg }
+export { assert, assertEquals, assertRejects, assertThrows, equal };
+export { assertSpyCall, assertSpyCalls, returnsArg, returnsNext, spy, stub };
 export { oauth1a };
 
 // Local Core Types
@@ -57,21 +89,53 @@ export { GitHubClient, GraphQlClient };
 
 // Local Core Models
 export type {
-	CommitModel, FileContentModel, GitHubVariablesModel, GitHubVarModel, IssueModel, LabelModel, MilestoneModel,
-	ProjectModel, PullRequestHeadOrBaseModel, PullRequestInfoModel, PullRequestModel, ReleaseModel, RepoModel,
-	TagModel, UserModel, WorkflowRunModel, WorkflowRunsModel, AssetModel
+	AssetModel,
+	CommitModel,
+	FileContentModel,
+	GitHubVariablesModel,
+	GitHubVarModel,
+	IssueModel,
+	LabelModel,
+	MilestoneModel,
+	ProjectModel,
+	PullRequestHeadOrBaseModel,
+	PullRequestInfoModel,
+	PullRequestModel,
+	ReleaseModel,
+	RepoModel,
+	TagModel,
+	UserModel,
+	WorkflowRunModel,
+	WorkflowRunsModel,
 };
 
 // Local GraphQL Models
-export type { 
-	ErrorModel, GitBranchModel,	GraphQlRequestResponseModel, PageInfoModel,
-	LocationModel,RawGetBranchTargetModel, RawGitBranchModel, RawRefsGetBranchModel
-}
+export type {
+	ErrorModel,
+	GitBranchModel,
+	GraphQlRequestResponseModel,
+	LocationModel,
+	PageInfoModel,
+	RawGetBranchTargetModel,
+	RawGitBranchModel,
+	RawRefsGetBranchModel,
+};
 
 // Local GitHub Client Errors
 export {
-	AuthError, GitError, IssueError, LabelError, MilestoneError, OrganizationError,
-	ProjectError, PullRequestError, ReleaseError, RepoError, TagError, UsersError, WorkflowError,
+	AuthError,
+	GitError,
+	IssueError,
+	LabelError,
+	MilestoneError,
+	OrganizationError,
+	ProjectError,
+	PullRequestError,
+	ReleaseError,
+	RepoError,
+	TagError,
+	UsersError,
+	WorkflowError,
 };
 
 // Package Client Errors
@@ -81,4 +145,4 @@ export { NuGetError };
 export { XError };
 
 // Local
-export { Utils, Guard, GitHubHttpStatusCodes };
+export { GitHubHttpStatusCodes, Guard, Utils };
