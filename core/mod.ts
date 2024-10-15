3dcbcde8ade6f91179e69e3a07d6f1981daa4686
CalvinWilkinson <kinsondigital@gmail.com>
2024-10-15 10:54:02 +0100

    chore: added types to mod file

--- core/mod.ts
+++ core/mod.ts
@@ -1,2 +1,9 @@
 export { GitHubClient } from "./GitHubClient.ts";
 export type { IssueOrPRRequestData } from "./IssueOrPRRequestData.ts";
+export type { ItemType } from "./Types.ts";
+export type { State } from "./Types.ts";
+export type { IssueOrPR } from "./Types.ts";
+export type { AnyBranch } from "./Types.ts";
+export type { GetDataFunc } from "./Types.ts";
+export type { BadCredentials } from "./Types.ts";
+export type { TransformType } from "./Types.ts";
